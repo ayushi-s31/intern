43ad12c239409a588b48a7a3ada323e391d10a86
ayushi-s31 <154958660+ayushi-s31@users.noreply.github.com>
2023-12-28 11:21:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const homeRouter = require('./routers/homeRouter')
+const port  = process.env.port || 8080;
 
-const App = () => {
-    const [transactions, setTransactions] = useState([]);
+const app  = express();
 
-    // Add transaction handling logic here
+mongoose.connect('mongodb://127.0.0.1:27017/studentsdata',{useNewUrlParser:true})
+const db = mongoose.connection;
 
-    return (
-        <div>
-            {/* Display transactions and other components */}
-        </div>
-    );
-};
+db.on("error",()=>{console.log("error in conection");})
+db.once('open',()=>{console.log("Connected");})
 
-ReactDOM.render(<App />, document.getElementById('app'));
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/', homeRouter)
+
+app.listen(port)
